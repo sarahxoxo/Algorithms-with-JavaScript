03850fcccd672ad137233fe82e4f74f57aa2ed22
chien <sarahxoxo@users.noreply.github.com>
2018-02-25 17:29:43 +0800

    add selection sort

---
+++ selectionsort.js
@@ -0,0 +1,20 @@
+var sort = function selectionSort(arr) {
+	for(var i = 0;i < arr.length;i++) {
+		//set minimum to this position
+		min = i;
+		//check the rest of the array to see if anything is smaller
+		for(var j = i + 1;j < arr.length;j++) {
+			if(arr[j] < arr[min]) {
+				min = j;
+			}
+		}
+		//if the minimum isn't in the position, swap it
+		if(i != min) {
+			//swap
+			var temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+		}
+	}
+	return arr;
+}
